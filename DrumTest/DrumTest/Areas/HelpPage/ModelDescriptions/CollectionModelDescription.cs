ce4d19c60ae1f1ebf0cd96ecd0e8268bcc139d82
Atchia <ibraahim.m.atchia@accenture.com>
2020-06-19 17:43:43 +0400

    Initialise solution + webapi MVC 4.6 Framework

---
+++ DrumTest/DrumTest/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace DrumTest.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ DrumTest/DrumTest/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DrumTest.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
